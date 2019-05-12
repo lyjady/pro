5e2c840acc4231899436ad6f2454467bfd3c6b1b
lyj <991296755@qq.com>
2019-05-12 19:56:25 +0800

    add new method

--- src/main/java/com/pro/origin/MyOrigin.java
+++ src/main/java/com/pro/origin/MyOrigin.java
@@ -4,4 +4,12 @@ public class MyOrigin {
     public static void main(String[] args) {
         System.out.println("Hello Origin");
     }
+
+    public static void doGet() {
+        System.out.println("MyOrigin.doGet");
+    }
+
+    public static void doPost() {
+        System.out.println("MyOrigin.doPost");
+    }
 }
